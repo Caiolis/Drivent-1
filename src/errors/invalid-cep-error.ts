6de10da238f7adf32246ec919b6ceee4e89d954a
Caio Carvalho <kaiogesteira@gmail.com>
2023-11-06 19:58:01 -0300

    feat: finished project

---
+++ src/errors/invalid-cep-error.ts
@@ -0,0 +1,8 @@
+import { ApplicationError } from '@/protocols';
+
+export function invalidCepError(): ApplicationError {
+    return {
+        name: 'Invalid Cep Error',
+        message: 'This Cep might not exist.'
+    };
+}
---
+++ src/routers/enrollments-router.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import { authenticateToken, validateBody } from '@/middlewares';
+import { getEnrollmentByUser, postCreateOrUpdateEnrollment, getAddressFromCEP } from '@/controllers';
+import { createOrUpdateEnrollmentSchema } from '@/schemas';
+
+const enrollmentsRouter = Router();
+
+enrollmentsRouter
+  .get('/cep', getAddressFromCEP)
+  .all('/*', authenticateToken)
+  .get('/', getEnrollmentByUser)
+  .post('/', validateBody(createOrUpdateEnrollmentSchema), postCreateOrUpdateEnrollment)
+  .post('/enrollments/cep', getAddressFromCEP);
+
+export { enrollmentsRouter };
