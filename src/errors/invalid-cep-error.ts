1b0c561566e889456d01169e30c64e1af05cfc8d
Caio Carvalho <kaiogesteira@gmail.com>
2024-01-02 15:54:21 -0300

    fix: issues fixes

--- src/errors/invalid-cep-error.ts
+++ src/errors/invalid-cep-error.ts
@@ -1,8 +1,8 @@
 import { ApplicationError } from '@/protocols';
 
 export function invalidCepError(): ApplicationError {
-    return {
-        name: 'Invalid Cep Error',
-        message: 'This Cep might not exist.'
-    };
+  return {
+    name: 'Invalid Cep Error',
+    message: 'This Cep might not exist.',
+  };
 }
